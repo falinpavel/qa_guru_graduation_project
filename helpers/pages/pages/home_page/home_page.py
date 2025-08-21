85d417259a6a4deb6a309b96fb98612b4b361030
Falin Pavel <falinpavel96@gmail.com>
2025-08-22 00:05:08 +0300

    fix test_change_location (added custom wait element)

--- helpers/pages/pages/home_page/home_page.py
+++ helpers/pages/pages/home_page/home_page.py
@@ -14,6 +14,7 @@ class HomePage:
     def open_with(self, location: str) -> 'HomePage':
         with allure.step(f'Открыть главную страницу для города {location}'):
             browser.open(f'{self.url}/{location}')
+
         with allure.step(f'Проверить что открыта главная страница для города {location}'):
             browser.should(have.url(f'{self.url}/{location}'))
         return self
@@ -21,12 +22,12 @@ class HomePage:
     @allure.step('Изменить город из главной страницы')
     def change_location(self, new_location: str) -> 'HomePage':
         with allure.step('Кликнуть кнопку-ссылку уже выбранного города'):
-            browser.element('.header__about-slogan-text').should(be.visible).should(
-                Condition.by_and(have.text("Доставка пиццы")))
+            browser.element('//span[@class="header__about-slogan-text"]').should(
+                Condition.by_and(be.visible, have.text("Доставка пиццы")))
 
-            browser.element('[data-testid="header__about-slogan-text_link"]').should(Condition.by_and(be.visible))
-            browser.element('[data-testid="header__about-slogan-text_link"]').should(
-                Condition.by_and(be.clickable)).click()
+            browser.element('.header__about-slogan-text_link').with_(timeout=browser.config.timeout * 2).should(
+                Condition.by_and(be.visible, be.clickable)
+            ).click()
 
         with allure.step('Ввести в поле поиска новый город'):
             browser.element('[data-testid="locality-selector-popup__search-input"]').should(
